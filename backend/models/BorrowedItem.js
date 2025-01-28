173a34b67e8c647030b0c6dbc1ce3665fab44098
keyframesfound <80465725+keyframesfound@users.noreply.github.com>
2025-01-29 00:18:46 +0800

    main

--- backend/models/BorrowedItem.js
+++ backend/models/BorrowedItem.js
@@ -1,14 +1,29 @@
-const mongoose = require('mongoose');
+const QRCode = require('qrcode');
 
-const borrowedItemSchema = new mongoose.Schema({
-    itemName: String,
-    borrowerName: String,
-    returnDate: Date,
-    borrowDate: Date,
-    isReturned: {
-        type: Boolean,
-        default: false
+class BorrowedItem {
+    constructor(itemName, borrowerName, returnDate, borrowDate, isReturned = false) {
+        this.itemName = itemName;
+        this.borrowerName = borrowerName;
+        this.returnDate = returnDate;
+        this.borrowDate = borrowDate;
+        this.isReturned = isReturned;
     }
-});
 
-module.exports = mongoose.model('BorrowedItem', borrowedItemSchema);
+    async generateQRCode() {
+        const data = {
+            itemName: this.itemName,
+            borrowerName: this.borrowerName,
+            returnDate: this.returnDate,
+            borrowDate: this.borrowDate,
+            isReturned: this.isReturned
+        };
+        try {
+            const url = await QRCode.toDataURL(JSON.stringify(data));
+            return url;
+        } catch (err) {
+            console.error(err);
+        }
+    }
+}
+
+module.exports = BorrowedItem;
