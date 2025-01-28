91d5e2c633c0701397276c48b97eed35b7842ff7
keyframesfound <80465725+keyframesfound@users.noreply.github.com>
2025-01-29 00:15:22 +0800

    main

--- backend/models/BorrowedItem.js
+++ backend/models/BorrowedItem.js
@@ -4,6 +4,7 @@ const borrowedItemSchema = new mongoose.Schema({
     itemName: String,
     borrowerName: String,
     returnDate: Date,
+    borrowDate: Date,
 });
 
 module.exports = mongoose.model('BorrowedItem', borrowedItemSchema);
